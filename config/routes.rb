b9d246c24b4c586eab61f33378701e46d387fa2b
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-10-25 02:12:06 +0300

    routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  # Configuración de rutas de Devise
+  devise_for :users, controllers: { registrations: "users/registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
+
+  # Defines the route for "splash/index"
+  get "/splash/index", to: "splash#index"
+
+  # Defines RESTful routes
+  resources :categories do
+    resources :transactions, only: [:index, :new, :create]
+  end
 end
